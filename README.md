84b29aeacb2bc6d7ccb8ce53691abfd97249e432
Matt Katz <mrmattkatz@gmail.com>
2015-12-11 12:04:46 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Challenge
+=========
+
+•	Fill a list (use whatever enumerable you would like Collection, Array, etc.) with random integers ranging 1 to 100
+•	Assign each of the items in the list a color Red, Yellow, White.  This is based on position 1 = Red, 2 = Yellow, 3 = White, and repeat (4 = Red, 5 = Yellow, etc.)
+•	Remove all of the even numbers that are red.  Remove all of the odd numbers that are yellow.  Remove all of the numbers divisible by 3 that are white.
+•	Sort the list ascending based on number and then color (Red < Yellow < White)
+•	Display the results
---
+++ number.js
@@ -0,0 +1,40 @@
+// Create arrays
+var array = [];
+var red = [];
+var yellow = [];
+var white = [];
+//Random numbers from 1 to 100
+for (var i = 0, l = 100; i < l; i++) {
+    array.push(Math.round(Math.random() * l));
+}
+//Posistion 1 is red
+for (var i = 0; i < array.length; i+=3){
+    //Remove even numbers
+    if (array[i] % 2) {
+        red.push(array[i]);
+    }
+}
+//Sort in ascending order
+red.sort(function(a, b){return a-b});
+//Position 2 is yellow
+for (var i = 1; i < array.length; i+=3){
+    //Remove odd numbers
+    if (! (array[i] % 2)) {
+        yellow.push(array[i]);
+    }
+}
+//Sort in ascending order
+yellow.sort(function(a, b){return a-b});
+//Position 3 is white
+for (var i = 2; i < array.length; i+=3){
+    //Remove if divisible by 3
+    if (array[i] % 3) {
+        white.push(array[i]);
+    }
+}
+//Sort in ascending order
+white.sort(function(a, b){return a-b});
+//red < yellow < white
+results = red.concat(yellow, white);
+//Display results
+console.log(results);
